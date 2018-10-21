5d64aeb2bd39e186b162585ba1c4a0e0650618e7
abraao.ribeiro <abraaohdg@gmail.com>
2018-10-20 21:24:29 -0300

    commit

--- vendas-ui/src/app/app.module.ts
+++ vendas-ui/src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
